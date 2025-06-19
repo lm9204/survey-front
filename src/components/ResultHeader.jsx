9b43a6ab0f06a0bb551690efc9224e2a7fbbb854
Choi Yeondeok <yeondeok99@naver.com>
2025-06-19 19:34:59 +0900

    hotfix: covert to string

--- src/components/ResultHeader.jsx
+++ src/components/ResultHeader.jsx
@@ -6,7 +6,7 @@ const ResultHeader = ({ typeData }) => (
     <h2 className={styles.title}>나의 유형은?</h2>
     <div className={styles.type}>{typeData?.name}</div>
     <img
-      src={`/images/activityi-types/${typeData?.type.toLowerCase()}-card.png`}
+      src={`/images/activityi-types/${String(typeData?.type).toLowerCase()}-card.png`}
       alt="유형 캐릭터"
       className={styles.character}
     />
